2a4a0e9a55d8eb712eb03b02dc48746e599b0eb0
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 01:55:36 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# First PyCharm Program to display "Hello World"
+# Robyn Lesch
+# 4 May 2020
+# Mood: excited 
+
+# basic introduction to python
+
+def make_excited(text):
+    return text.upper() + "!!"
+
+
+hello = "hello world"
+excitedHello = make_excited(hello)
+print(make_excited(hello))
