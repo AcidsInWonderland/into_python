919b1670953661350eaaeee23558c922059d5688
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 01:57:04 +0200

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# Madlibs Game: Excuses
+# Robyn Lesch
+# 4 May 2020
+# Mood: confident 
+
+# user inputs to make excused note
+name = input("Enter a name: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+your_name = input("Enter your name: ")
+
+# printing the note 
+print("------------")
+print("EXCUSED")
+print("Please excuse " + name)
+print("who is too " + adjective)
+print("to attend " + noun + " class")
+print("Signed: " + your_name)
