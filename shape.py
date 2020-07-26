ffdcaea3383c96af2923a620c12685ca6f90e921
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 01:56:16 +0200

    Create shape.py

---
+++ shape.py
@@ -0,0 +1,12 @@
+# Shape Drawing
+# Robyn Lesch
+# 4 May 2020
+# Mood: Yay!
+
+# prints basic shape 
+print("**************************")
+print("*                        *")
+print("*                        *")
+print("*                        *")
+print("*                        *")
+print("**************************")
