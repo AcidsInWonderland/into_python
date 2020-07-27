afa3160dc7e09867cdfaf0643ff976507ad754dc
Robyn Lesch <67701622+AcidsInWonderland@users.noreply.github.com>
2020-07-27 02:07:27 +0200

    Create rabbit.py

---
+++ Week2/rabbit.py
@@ -0,0 +1,13 @@
+# Ascii Art - Rabbit
+# Robyn Lesch
+# 7 May 2020
+# Mood: awe
+
+# learning how to print art using python
+
+print("(\\\                             (\ /)")
+print("( ' ')         (\ _ /)         (. . )      //) ")
+print(" ")
+print("O('')('')     (\ '.' /)      ('')('')O  ('' )        ")
+print(" ")
+print("              ('')_('')                 () ()O  ")
